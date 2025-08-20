6af5a800ccceb4b43d5b24be03138f0c91c1f706
Polsa229 <hodonouola@gmail.com>
2025-08-20 14:00:10 +0100

    center text of btn 'Download the app'

--- src/Components/CallToActionBtn.jsx
+++ src/Components/CallToActionBtn.jsx
@@ -16,7 +16,7 @@ const CallToActionBtn = ({ hero = false }) => {
     return (
         <div className="flex flex-wrap justify-center gap-4">
             <Link to="/#"
-                className={`w-full md:w-auto items-center py-2 px-4 md:px-8 text-black text-base font-bold nded-full overflow-hidden bg-white rounded-full transition-all duration-400 ease-in-out shadow-md hover:scale-105 hover:text-white hover:shadow-lg active:scale-90 before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-gradient-to-r before:from-secondary before:to-secondary before:transition-all before:duration-500 before:ease-in-out before:z-[-1] before:rounded-full hover:before:left-0`}
+                className={`w-full md:w-auto flex  items-center py-2 px-4 md:px-8 text-black text-base font-bold overflow-hidden bg-white rounded-full transition-all duration-400 ease-in-out shadow-md hover:scale-105 hover:text-white hover:shadow-lg active:scale-90 before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-gradient-to-r before:from-secondary before:to-secondary before:transition-all before:duration-500 before:ease-in-out before:z-[-1] before:rounded-full hover:before:left-0`}
             >
                 Download the app
             </Link>
