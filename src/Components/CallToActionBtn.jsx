5a6661559bfe34fa09ef648baac253c6080e82e4
Polsa229 <hodonouola@gmail.com>
2025-08-29 23:12:41 +0100

    add scroll on call to action btn

--- src/Components/CallToActionBtn.jsx
+++ src/Components/CallToActionBtn.jsx
@@ -1,12 +1,18 @@
+import { useNavigate } from "react-router-dom";
+import { scrollToSection } from "../functions/scrollingFunctions";
 
 const CallToActionBtn = () => {
+    const navigate = useNavigate();
     return (
 
         <div className="flex gap-4 justify-center">
             <button className="bg-primary text-white hover:bg-secondary500 hover:text-primary px-4 py-2 rounded-full transition">
                 Get a Free Demo
             </button>
-            <button className="bg-secondary500 text-white px-4 py-2 rounded-full hover:bg-white hover:text-secondary500 transition">
+            <button
+                className="bg-secondary500 text-white px-4 py-2 rounded-full hover:bg-white hover:text-secondary500 transition"
+                onClick={() => scrollToSection(navigate, "/", "pricing")}
+            >
                 See Pricing
             </button>
         </div>
